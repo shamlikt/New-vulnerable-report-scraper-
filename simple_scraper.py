abc8798bbc09ea204642cfb72e5ac4ef29bc6765
shamlik <tshamlikt@gmail.com>
2017-03-24 21:28:58 +0530

    Adds comments to the code

--- simple_scraper.py
+++ simple_scraper.py
@@ -4,7 +4,10 @@ from requests import get
 from bs4 import BeautifulSoup
 
 class DataScraper:
+    
     def __init__(self):
+        ''' Initializing data structure, self.value is a python dict
+       which is used for storing fields of excel table '''
         self.value ={'val_name': '--',
                     'severity' : '--',
                     'date' : '--',
@@ -15,24 +18,27 @@ class DataScraper:
         self.data = []
 
     def get_html_data(self, url):
+         ''' Method to fetch html data and return a Beutifulsoup object'''
          return BeautifulSoup(get(url).text, "html.parser")
 
     def scrape_kb_crt(self, url):
+        ''' This method is used for parsing www.kb.cert.or'''
         data = self.get_html_data(url)
-        lists = data.find(id="list-of-vuls").find_all("li")
+        lists = data.find(id="list-of-vuls").find_all("li") # Selecting list of valuns from https://www.kb.cert.org/vuls/
         for li in lists:
-            temp_data = deepcopy(self.value)
-            temp_data['val_name'] = li.find("span", class_="vul-title truncate").text
-            temp_data['date'] = li.find("span", class_="vul-date").text
+            temp_data = deepcopy(self.value)                # creating copy of self.value 
+            temp_data['val_name'] = li.find("span", class_="vul-title truncate").text # parsing name using class name of span
+            temp_data['date'] = li.find("span", class_="vul-date").text  # parsing published using class name of span 
 
-            page_link = "{}{}".format(url.strip('/vuls/'),li.a['href'])
-            new_data = self.get_html_data(page_link).find(id="vulnerability-note-content")
+            page_link = "{}{}".format(url.strip('/vuls/'),li.a['href'])   # Creating link address 
+            new_data = self.get_html_data(page_link).find(id="vulnerability-note-content") # fetching link data and selecting a specific div using id 
             temp_data['description'] = new_data.p.text
-            temp_data['solution'] = new_data.find_all("table")[2].find("tr").text
-            self.data.append(temp_data)
+            temp_data['solution'] = new_data.find_all("table")[2].find("tr").text # selecting solution part from html page using 'tr' tabs 
+            self.data.append(temp_data) # appending temp data info to class variable called self.data
 
 
 def write_data(file_name, data):
+    ''' Method used for writing data into .xls file '''
     book = xlwt.Workbook()
     sheet1 = book.add_sheet("sheet1")
     cols = ["A", "B", "C", "D", "E", "F"]
